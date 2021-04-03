fa674b976ac18059520e04ed17499f4365bf628b
dodamtanguri <sh663746@gmail.com>
2021-04-04 00:58:39 +0900

    예약 취소하기 테스트 완료

--- src/test/java/kr/or/connect/reservation/controller/api/ReservationApiControllerTest.java
+++ src/test/java/kr/or/connect/reservation/controller/api/ReservationApiControllerTest.java
@@ -2,8 +2,12 @@ package kr.or.connect.reservation.controller.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.or.connect.reservation.config.ApplicationConfig;
+import kr.or.connect.reservation.dto.Body.CancelBody;
 import kr.or.connect.reservation.dto.Body.ReservationBody;
 import kr.or.connect.reservation.dto.Body.ReservationPriceBody;
+import kr.or.connect.reservation.dto.CancelReservation;
+import kr.or.connect.reservation.dto.ReservationInfos;
+import kr.or.connect.reservation.dto.api.GetReservationInfoApiDTO;
 import kr.or.connect.reservation.service.ReservationService;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,16 +22,20 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -77,22 +85,67 @@ public class ReservationApiControllerTest {
                         .accept(MediaType.APPLICATION_JSON_VALUE)
                         .content(new ObjectMapper().writeValueAsString(body))
                         .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(MockMvcResultHandlers.print())
+        ).andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
 
     }
 
     @Test
+    @WithMockCustomUser
     @DisplayName("예약 조회 하기")
     public void getReservation() throws Exception {
-        
+
+        ReservationInfos items = new ReservationInfos();
+        items.setId(51);
+        items.setProductId(1);
+        items.setDisplayInfoId(1);
+        items.setCancelFlag(0);
+        items.setProductDescription("PRODUCT DESCRIPTION");
+        items.setProductContent("PRODUCT CONTENT");
+        items.setUserId(1);
+        items.setSumPrice(20000);
+        items.setReservationDate(new Date());
+        items.setCreateDate(new Date());
+        items.setModifyDate(new Date());
+        List<ReservationInfos> itemList = Arrays.asList(items);
+        GetReservationInfoApiDTO getReservation = new GetReservationInfoApiDTO();
+        getReservation.setItems(itemList);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/reservationInfos")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        when(service.getReservation(items.getUserId())).thenReturn(getReservation);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk()).andDo(print());
+
+        verify(service).getReservation(items.getUserId());
+
 
     }
 
     @Test
+    @WithMockCustomUser
     @DisplayName("예약 취소 하기")
-    public void deleteReservation() throws Exception {
+    public void cancelReservation() throws Exception {
+        CancelBody cancelBody = new CancelBody();
+        cancelBody.setId(51);
+        CancelReservation cancelReservation = new CancelReservation();
+        cancelReservation.setResult("Success");
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/api/reservationInfos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(new ObjectMapper().writeValueAsString(cancelBody));
+
+        when(service.cancelReservation(cancelBody, 1)).thenReturn(cancelReservation);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andDo(print());
+
 
     }
 }
