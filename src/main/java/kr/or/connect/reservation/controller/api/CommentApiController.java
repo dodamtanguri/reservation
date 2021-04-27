084804e394e95543848fe6289184317ecee34a52
dodamtanguri <sh663746@gmail.com>
2021-04-27 23:33:59 +0900

    댓글등록하기 테스트 중

--- src/main/java/kr/or/connect/reservation/controller/api/CommentApiController.java
+++ src/main/java/kr/or/connect/reservation/controller/api/CommentApiController.java
@@ -26,7 +26,7 @@ public class CommentApiController {
             @ApiResponse(code = 500, message = "Exception")
     })
     @GetMapping
-    public CommentApiDTO commentApitDTO(
+    public CommentApiDTO commentApiDTO(
             @RequestParam(name = "START", required = false, defaultValue = "0") int start,
             @RequestParam(name = "productId", required = false, defaultValue = "1") int productId) {
         CustomUserDetails customUserDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -35,8 +35,7 @@ public class CommentApiController {
     }
 
     @PostMapping
-    public PostCommentApiDTO postCommentDTO(
-
+    public PostCommentApiDTO addCommentDTO(
             @RequestParam(name = "reservationInfoId", required = true, defaultValue = "1") int reservationInfoId,
             @RequestParam(name = "score", required = true, defaultValue = "3") int score,
             @RequestParam(name = "comment", required = true, defaultValue = "댓글을 저장합니다.") String comment,
@@ -44,7 +43,6 @@ public class CommentApiController {
     ) throws Exception {
         CustomUserDetails customUserDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         int userID = customUserDetails.getUserId();
-
         return commentService.insertComments(reservationInfoId, score, comment, userID, file);
     }
 }
--- src/test/java/kr/or/connect/reservation/controller/api/CommentApiControllerTest.java
+++ src/test/java/kr/or/connect/reservation/controller/api/CommentApiControllerTest.java
@@ -1,20 +1,49 @@
 package kr.or.connect.reservation.controller.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import kr.or.connect.reservation.config.ApplicationConfig;
+import kr.or.connect.reservation.dto.CommentDTO;
+import kr.or.connect.reservation.dto.CommentImagesDTO;
+import kr.or.connect.reservation.dto.api.CommentApiDTO;
 import kr.or.connect.reservation.service.CommentService;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.web.method.annotation.AuthenticationPrincipalArgumentResolver;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import java.io.FileInputStream;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = ApplicationConfig.class)
 @WebAppConfiguration
 public class CommentApiControllerTest {
+
     @InjectMocks
-    public CommentApiController commentApiController;
+    public CommentApiController controller;
     @Mock
     CommentService commentService;
     private MockMvc mockMvc;
@@ -22,46 +51,94 @@ public class CommentApiControllerTest {
     @Before
     public void createController() {
         MockitoAnnotations.initMocks(this);
-        this.mockMvc = MockMvcBuilders.standaloneSetup(commentApiController).addFilters(new CharacterEncodingFilter("UTF-8", true)).build();
+        this.mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .alwaysExpect(status().isOk())
+                .setCustomArgumentResolvers(new AuthenticationPrincipalArgumentResolver())
+                .addFilters(new CharacterEncodingFilter("UTF-8", true)).build();
+    }
+
+
+    @Test
+    @WithMockCustomUser
+    @DisplayName("댓글 조회 하기")
+    public void getComment() throws Exception {
+        CommentDTO commentDTO = CommentDTO.builder()
+                .id(5050)
+                .productId(1)
+                .reservationInfoId(6060)
+                .score(3)
+                .comment("댓글 조회 하기 테스트중입니다.")
+                .userId(1)
+                .build();
+        CommentImagesDTO commentImagesDTO = CommentImagesDTO.builder()
+                .id(1111)
+                .reservationId(6060)
+                .reservationUserCommentId(5050)
+                .fileId(3333)
+                .fileName("FILE NAME")
+                .saveFileName("SAVE FILE NAME")
+                .contentType("image/png")
+                .createDate(LocalDateTime.now())
+                .modifyDate(LocalDateTime.now())
+                .build();
+
+        List<CommentImagesDTO> ImageList = new ArrayList<>();
+        ImageList.add(commentImagesDTO);
+        commentDTO.setReservationUserCommentImages(ImageList);
+
+        List<CommentDTO> commentList = new ArrayList<>();
+        commentList.add(commentDTO);
+
+        CommentApiDTO comment = new CommentApiDTO();
+        comment.setCommentCount(5);
+        comment.setTotalCount(5);
+        comment.setReservationUserComments(commentList);
+
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/comments")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        when(commentService.getComment(1, 0, 1)).thenReturn(comment);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk()).andDo(print());
+
+        verify(commentService).getComment(1, 0, 1);
+
     }
 
     @Test
-    public void getCommentImgae() throws Exception {
-
-//        CommentImagesDTO imagesDTO = new CommentImagesDTO();
-//        imagesDTO.setCommentImageId(1111);
-//        imagesDTO.setReservationId(6060);
-//        imagesDTO.setCommentId(5050);
-//        imagesDTO.setFileId(7070);
-//        List<CommentImagesDTO> imagesDTOList = Arrays.asList(imagesDTO);
-//        CommentDTO commentDTO = new CommentDTO();
-//        commentDTO.setReservationUserCommentImages(imagesDTOList);
-//
-//        CommentDTO dto = new CommentDTO();
-//        dto.setId(5050);
-//        dto.setProductId(1010);
-//        dto.setReservationInfoId(6060);
-//        dto.setScore(4);
-//        dto.setReservationUserCommentImages(imagesDTOList);
-//        dto.setReservationEmail("email");
-//        dto.setComment("최고!");
-//        dto.setCreateDate("등록일");
-//        dto.setModifyDate("수정일");
-//
-//        List<CommentDTO> commentDTOS = Arrays.asList(dto);
-//        CommentApiDTO commentApiDTO = new CommentApiDTO();
-//        commentApiDTO.setReservationUserComments(commentDTOS);
-//        commentApiDTO.setCommentCount(15);
-//        commentApiDTO.setTotalCount(5);
-//
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/displayinfos/comment").contentType(MediaType.APPLICATION_JSON);
-//        when(commentService.getComment(1, 0)).thenReturn(commentApiDTO);
-//
-//
-//        mockMvc.perform(requestBuilder).andExpect(status().isOk()).andDo(print());
-//        verify(commentService).getComment(1, 0);
+    @DisplayName("댓글 등록 하기")
+    public void addComment() throws Exception {
 
 
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+        FileInputStream fis = new FileInputStream("/Users/BOOST/temp/부스트코스.png");
+        MockMultipartFile file = new MockMultipartFile("file",fis);
+
+
+        HashMap<String, String> contentTypeParams = new HashMap<String, String>();
+        contentTypeParams.put("boundary", "265001916915724");
+        MediaType mediaType = new MediaType("multipart", "form-data", contentTypeParams);
+
+
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                        .multipart("/api/comments")
+                        .accept(MediaType.MULTIPART_FORM_DATA_VALUE)
+                        .param("reservationInfoId", "1")
+                        .param("score", "3")
+                        .param("comment", "댓글을 저장합니다.")
+                        .param("file", file.getName())
+                        .contentType(mediaType)
+
+        ).andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
     }
 
 }
